daa93f2e9250cc42a546a33fb69b2704e278267b
Dinesh Kumar <m22aie227@iitj.ac.in>
2023-09-07 23:06:24 +0530

    testing action again

--- exp.py
+++ exp.py
@@ -15,8 +15,9 @@ import matplotlib.pyplot as plt
 from sklearn import metrics
 
 #utils import
-from utils import load_dataset, data_preprocessing, train_model, get_list_of_param_comination, tune_hparams
-from utils import split_train_dev_test, predict_and_eval, visualize_first_n_sample_prediction, get_classification_report
+from utils import load_dataset, data_preprocessing, split_train_dev_test,predict_and_eval
+from utils import get_list_of_param_comination, tune_hparams
+ 
 
 
 ###########################################################################################
@@ -27,8 +28,7 @@ X,y = load_dataset()
 
 ################################################################################################
 #3. Spliting the data
-# X_train, X_test, y_train, y_test = train_test_spliting(X,y)   
-X_train, y_train, X_test, y_test, X_dev, y_dev = split_train_dev_test(X, y, test_size=0.2, dev_size=0.25)  
+X_train, y_train, X_test, y_test, X_dev, y_dev = split_train_dev_test(X, y, test_size=0.2, dev_size=0.2)  
 
 
 #################################################################################################
@@ -47,7 +47,7 @@ list_of_param_comination = get_list_of_param_comination([gamma_values, C_values]
 
 best_hparams, best_model, best_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_param_comination)
 
-print(best_hparams, best_model, best_accuracy)
+print(best_hparams, best_accuracy)
 
 
 
@@ -56,47 +56,3 @@ print(best_hparams, best_model, best_accuracy)
 # test accuracy
 test_accuracy, y_pred = predict_and_eval(best_model, X_test, y_test)
 print("accuracy of model on test sat is ", test_accuracy)
-
-# Below we visualize the first 4 test samples and show their predicted
-# visualize_first_n_sample_prediction(X_test, y_pred, n = 4)
-
-#print classification report
-classification_report = get_classification_report(y_test, y_pred)
-print(f"Classification report for classifier {best_model}")
-print(classification_report)
-
-
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit values and the predicted digit values.
-
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
-# disp.figure_.suptitle("Confusion Matrix")
-# print(f"Confusion matrix:\n{disp.confusion_matrix}")
-# plt.show()
-
-
-
-
-###############################################################################
-# If the results from evaluating a classifier are stored in the form of a
-# :ref:`confusion matrix <confusion_matrix>` and not in terms of `y_true` and
-# `y_pred`, one can still build a :func:`~sklearn.metrics.classification_report`
-# as follows:
-
-
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
-
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
-
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
--- utils.py
+++ utils.py
@@ -19,13 +19,13 @@ def data_preprocessing(data):
     return data
 
 #spliting data 
-def train_test_spliting(X,y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
-    return  X_train, X_test, y_train, y_test
+# def train_test_spliting(X,y):
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
+#     return  X_train, X_test, y_train, y_test
 
 def split_train_dev_test(X, y, test_size, dev_size):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size, random_state=10)
-    X_train, X_dev, y_train, y_dev = train_test_split(X_train, y_train, test_size = dev_size, random_state=10)
+    X_train, X_dev, y_train, y_dev = train_test_split(X_train, y_train, test_size = dev_size/(1-test_size), random_state=10)
     return X_train, y_train, X_test, y_test, X_dev, y_dev
 
 
@@ -41,22 +41,22 @@ def train_model(X_train, y_train, model_params, model_type = 'svm'):
 #prediction and accuracy evaluation
 def predict_and_eval(model, X_test, y_test):
     predicted = model.predict(X_test)
-    accuracy = round(metrics.accuracy_score(y_test, predicted), 4) * 100
+    accuracy = metrics.accuracy_score(y_test, predicted) * 100
     return accuracy, predicted
 
 
 #Visualize first n sample and show their prediction
-def visualize_first_n_sample_prediction(X_test, y_pred, n = 4):
-    _, axes = plt.subplots(nrows=1, ncols=n, figsize=(10, 3))
-    for ax, image, prediction in zip(axes, X_test, y_pred):
-        ax.set_axis_off()
-        image = image.reshape(8, 8)
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title(f"Prediction: {prediction}")
+# def visualize_first_n_sample_prediction(X_test, y_pred, n = 4):
+#     _, axes = plt.subplots(nrows=1, ncols=n, figsize=(10, 3))
+#     for ax, image, prediction in zip(axes, X_test, y_pred):
+#         ax.set_axis_off()
+#         image = image.reshape(8, 8)
+#         ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+#         ax.set_title(f"Prediction: {prediction}")
 
 #return classification report
-def get_classification_report(y_test, y_pred):
-    return metrics.classification_report(y_test, y_pred)
+# def get_classification_report(y_test, y_pred):
+#     return metrics.classification_report(y_test, y_pred)
 
 
 #this is done in two for loops, irespective of number of params
@@ -68,8 +68,8 @@ def get_list_of_param_comination(list_of_param, param_names):
         for i in range(len(list_of_param)):
             comb[param_names[i]] = each[i]
         list_of_param_comination.append(comb)
-
     return list_of_param_comination
+
 
 ## hparams tuning function as per assignment 3
 def tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination):
