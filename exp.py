f91aca798a5b86d33bb981b6d938c2658c54f210
Dinesh Kumar <m22aie227@iitj.ac.in>
2023-10-07 20:10:49 +0530

    svm and tree model comaprison on multiple runs

--- exp.py
+++ exp.py
@@ -7,7 +7,7 @@ digit classification model code for mlops
 #utils import
 from utils import load_dataset, data_preprocessing, split_train_dev_test,predict_and_eval
 from utils import get_list_of_param_comination, tune_hparams
- 
+import pandas as pd 
 
 
 ###########################################################################################
@@ -18,37 +18,64 @@ X,y = load_dataset()
 
 ################################################################################################
 #taking different combinations of train dev and test and reporting results
-#3. Spliting the data
-test_size =  [0.1, 0.2, 0.3]
-dev_size = [0.1, 0.2, 0.3]
-for ts in test_size:
-    for ds in dev_size:
-        X_train, y_train, X_test, y_test, X_dev, y_dev = split_train_dev_test(X, y, test_size=ts, dev_size=ds)  
 
+test_size =  [0.2]
+dev_size = [0.2]
+total_run = 5
+results = []
+for run_num in range(total_run):
+    for ts in test_size:
+        for ds in dev_size:
+            #3. Spliting the data
+            X_train, y_train, X_test, y_test, X_dev, y_dev = split_train_dev_test(X, y, test_size=ts, dev_size=ds)  
 
-        #################################################################################################
-        #4. Preprocessing the data
-        X_train = data_preprocessing(X_train)
-        X_test= data_preprocessing(X_test)
-        X_dev =data_preprocessing(X_dev)
+            #################################################################################################
+            #4. Preprocessing the data
+            X_train = data_preprocessing(X_train)
+            X_test= data_preprocessing(X_test)
+            X_dev =data_preprocessing(X_dev)
 
-                                                                                                                    
-        #################################################################################################
-        #5. Classification model training
-        #hyper parameter tuning for gamma and C
-        gamma_values = [0.001, 0.002, 0.005, 0.01, 0.02]
-        C_values = [0.1, 0.2, 0.5, 0.75, 1]
-        list_of_param_comination = get_list_of_param_comination([gamma_values, C_values],  ['gamma', 'C'])
-        best_hparams, best_model, best_val_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_param_comination)
+            #################################################################################################
+            #5. Classification model training
+            #5.1 SVM
+            #hyper parameter tuning for gamma and C
+            model_type = 'svm'
+            gamma_values = [0.001, 0.002, 0.005, 0.01, 0.02]
+            C_values = [0.1, 0.2, 0.5, 0.75, 1]
+            list_of_param_comination = get_list_of_param_comination([gamma_values, C_values],  ['gamma', 'C'])
+            best_hparams, best_model, best_val_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_param_comination, model_type = model_type)
 
-        #get training accuracy of this best model:
-        train_accuracy, _ = predict_and_eval(best_model, X_train, y_train)
+            #get training accuracy of this best model:
+            train_accuracy, _ = predict_and_eval(best_model, X_train, y_train)
 
-        ################################################################################################
-        #6. Prediction and evaluation on test sat
-        # test accuracy
-        test_accuracy, _ = predict_and_eval(best_model, X_test, y_test)
+            ################################################################################################
+            #6. Prediction and evaluation on test sat
+            # test accuracy
+            test_accuracy, _ = predict_and_eval(best_model, X_test, y_test)
 
-        #print for github actions
-        print('test_size=',ts,' dev_size=',ds,' train_size=',round(1-ts-ds,2),' train_acc=',train_accuracy,' dev_acc',best_val_accuracy,' test_acc=',test_accuracy, ' best_hyper_params=', best_hparams)
-       
+            #print for github actions
+            print('svm  ','test_size=',ts,' dev_size=',ds,' train_size=',round(1-ts-ds,2),' train_acc=',train_accuracy,' dev_acc',best_val_accuracy,' test_acc=',test_accuracy, ' best_hyper_params=', best_hparams)
+            results.append({'run_num':run_num,'model_type':model_type, 'train_accuracy':train_accuracy, 'val_accuracy':best_val_accuracy,
+                            'test_acc':test_accuracy, 'best_hparams':best_hparams})
+        #5.2 Decision Tree
+            #hyper parameter tunning
+            model_type = 'tree'
+            max_depth = [5,10,20,30,50,75,100]
+            list_of_param_comination = get_list_of_param_comination([max_depth],  ['max_depth'])
+            best_hparams, best_model, best_val_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_param_comination,model_type = model_type)
+
+            #get training accuracy of this best model:
+            train_accuracy, _ = predict_and_eval(best_model, X_train, y_train)
+
+            ################################################################################################
+            #6. Prediction and evaluation on test sat
+            # test accuracy
+            test_accuracy, _ = predict_and_eval(best_model, X_test, y_test)
+
+            #print for github actions
+            print('tree ','test_size=',ts,' dev_size=',ds,' train_size=',round(1-ts-ds,2),' train_acc=',train_accuracy,' dev_acc',best_val_accuracy,' test_acc=',test_accuracy, ' best_hyper_params=', best_hparams)
+            results.append({'run_num':run_num,'model_type':model_type, 'train_accuracy':train_accuracy, 'val_accuracy':best_val_accuracy,
+                            'test_acc':test_accuracy, 'best_hparams':best_hparams})
+
+result_df = pd.DataFrame(results)
+print(result_df.groupby('model_type').describe().T)
