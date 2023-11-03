ccb27e64e499666aa7d59719356a8b64519d1364
Dinesh Kumar <m22aie227@iitj.ac.in>
2023-11-03 21:23:42 +0530

    docker run script file updatd

--- docker_run.sh
+++ docker_run.sh
@@ -10,7 +10,7 @@ sudo docker build -t docker_assigment:v1 -f docker/Dockerfile .
 
 ######below 2 commands are updated for doing same work as above but from github action.
 #docker run command that mount volume, run it in interactive mode and take python script args as well to trai svm model
-sudo docker run -v /home/dineshkumar/Codes/digit-classification/models:/digits/models -T docker_assigment:v1 --total_run 1 --dev_size 0.2 --test_size 0.2 --model_type 'svm' 
+sudo docker run -v /home/dineshkumar/Codes/digit-classification/models:/digits/models  docker_assigment:v1 --total_run 1 --dev_size 0.2 --test_size 0.2 --model_type 'svm' 
 #same docker run command to train tree model
-sudo docker run -v /home/dineshkumar/Codes/digit-classification/models:/digits/models -T docker_assigment:v1 --total_run 1 --dev_size 0.2 --test_size 0.2 --model_type 'tree'
+sudo docker run -v /home/dineshkumar/Codes/digit-classification/models:/digits/models  docker_assigment:v1 --total_run 1 --dev_size 0.2 --test_size 0.2 --model_type 'tree'
 
