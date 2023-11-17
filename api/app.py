077f5d43ed0e2bf8f5abe877bed5af9a85d4050f
Dinesh Kumar <m22aie227@iitj.ac.in>
2023-11-17 22:28:12 +0530

    serving model via flask on azure cloud using container

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
 from flask import Flask,request
+import numpy as np
+from joblib import  load
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>This is home page for flask app to serve model on azure cloud</p>"
 
 @app.route('/user/<username>')
 def profile(username):
@@ -20,8 +22,19 @@ def sum_two_numbers(x,y):
 @app.route("/predict", methods = ['POST'])
 def predict_fn():
     input_data = request.get_json()
-    x = input_data['x']
-    y = input_data['y']
-    # res = int(x) + int(y)
-    return f"sum of {x} and {y} is {x + y}"
+    img = input_data['image']
 
+    img = list(map(float, img))
+
+    img = np.array(img).reshape(1,-1)
+
+    model = load("svm_model.joblib")
+    predicted = model.predict(img)
+    return f"Prediction : uploaded images belongs to number {predicted[0]} category"
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 80)
+
+
+    
